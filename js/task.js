e7a564fe6640b450172d8f13a6367771044e6ec7
Yama Dwi Yulianto <yamadwiyanto31@gmail.com>
2026-06-29 23:20:27 +0700

    membuat pop up edit delete pada task

--- js/task.js
+++ js/task.js
@@ -16,6 +16,16 @@ const completedContainer = document.getElementById("completedContainer");
 
 const deleteAllBtn = document.getElementById("deleteAllBtn");
 
+const completedAccordion = document.getElementById("completedAccordion");
+
+const taskMenu = document.getElementById("taskMenu");
+
+const editTaskBtn = document.getElementById("editTaskBtn");
+
+const deleteTaskBtn = document.getElementById("deleteTaskBtn");
+
+let selectedTaskId = null;
+
 
 // =========================
 // INIT
@@ -24,6 +34,21 @@ const deleteAllBtn = document.getElementById("deleteAllBtn");
 function initTask() {
 
     addTaskBtn.addEventListener("click", saveTask);
+
+    completedAccordion.addEventListener(
+        "click",
+        toggleCompleted
+    );
+
+    editTaskBtn.addEventListener(
+        "click",
+        updateTask
+    );
+    
+    deleteTaskBtn.addEventListener(
+        "click",
+        deleteTask
+    );
 
 }
 
@@ -82,9 +107,19 @@ function saveTask() {
 
 }
 
+function getPriority(priorityId) {
+
+    return appData.priorities.find(
+        priority => priority.id === priorityId
+    );
+
+}
+
 function renderTask() {
 
     todoContainer.innerHTML = "";
+
+    completedContainer.innerHTML = "";
 
     const tasks = appData.tasks.filter(task =>
 
@@ -95,14 +130,226 @@ function renderTask() {
 
     tasks.forEach(task => {
 
+        const priority = getPriority(task.priorityId);
+
+        // =========================
+        // Container
+        // =========================
+
         const item = document.createElement("div");
 
         item.classList.add("task-item");
 
-        item.textContent = task.title;
+        // =========================
+        // Left
+        // =========================
+
+        const left = document.createElement("div");
+
+        left.classList.add("task-left");
+
+        // Checkbox
+
+        const checkbox = document.createElement("input");
+
+        checkbox.type = "checkbox";
+
+        checkbox.classList.add("task-check");
+        
+        checkbox.checked = task.completed;
+
+        checkbox.addEventListener("change", () => {
+
+            completeTask(task.id);
+        
+        });
+
+        // Info
+
+        const info = document.createElement("div");
+
+        info.classList.add("task-info");
+
+        // Title
+
+        const title = document.createElement("div");
+
+        title.classList.add("task-title");
+
+        title.textContent = task.title;
+
+        info.appendChild(title);
+
+        left.appendChild(checkbox);
+
+        left.appendChild(info);
+
+        // =========================
+        // Right
+        // =========================
+
+        const right = document.createElement("div");
+
+        right.classList.add("task-right");
+
+        // Flag
+
+        const flag = document.createElement("img");
+
+        flag.classList.add("task-flag");
+
+        if (priority) {
+
+            if (priority.color === "#EF4444") {
+
+                flag.src = "assets/red-flag.svg";
+
+            }
+
+            else if (priority.color === "#F59B0B") {
+
+                flag.src = "assets/yellow-flag.svg";
+
+            }
+
+            else {
+
+                flag.src = "assets/blue-flag.svg";
+
+            }
+
+        }
+
+        // Menu
+
+        const menu = document.createElement("button");
+
+        menu.classList.add("task-more");
+
+        const menuIcon = document.createElement("img");
+
+        menuIcon.src = "assets/three-dot.svg";
+
+        menu.appendChild(menuIcon);
+
+        menu.addEventListener("click", (event) => {
+
+            event.stopPropagation();
+        
+            selectedTaskId = task.id;
+        
+            taskMenu.classList.add("show");
+        
+            taskMenu.style.left = event.pageX + "px";
+        
+            taskMenu.style.top = event.pageY + "px";
+        
+        });
+
+        right.appendChild(flag);
+
+        right.appendChild(menu);
+
+        // =========================
+
+        item.appendChild(left);
+
+        item.appendChild(right);
 
         todoContainer.appendChild(item);
 
+    });
+
+    const completedTasks = appData.tasks.filter(task =>
+
+        task.listId === appData.currentList &&
+        task.completed
+    
+    );
+    
+    completedTasks.forEach(task => {
+    
+        const priority = getPriority(task.priorityId);
+    
+        const item = document.createElement("div");
+        item.classList.add("task-item");
+    
+        // LEFT
+        const left = document.createElement("div");
+        left.classList.add("task-left");
+    
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("task-check");
+        checkbox.checked = true;
+    
+        checkbox.addEventListener("change", () => {
+    
+            completeTask(task.id);
+    
+        });
+    
+        const info = document.createElement("div");
+        info.classList.add("task-info");
+    
+        const title = document.createElement("div");
+        title.classList.add("task-title");
+        title.textContent = task.title;
+    
+        info.appendChild(title);
+    
+        left.appendChild(checkbox);
+        left.appendChild(info);
+    
+        // RIGHT
+        const right = document.createElement("div");
+        right.classList.add("task-right");
+    
+        if(priority){
+    
+            const flag = document.createElement("img");
+    
+            flag.classList.add("task-flag");
+    
+            if(priority.color === "#EF4444"){
+    
+                flag.src = "assets/red-flag.svg";
+    
+            }
+    
+            else if(priority.color === "#F59B0B"){
+    
+                flag.src = "assets/yellow-flag.svg";
+    
+            }
+    
+            else{
+    
+                flag.src = "assets/blue-flag.svg";
+    
+            }
+    
+            right.appendChild(flag);
+    
+        }
+    
+        const menu = document.createElement("button");
+    
+        menu.classList.add("task-more");
+    
+        const menuIcon = document.createElement("img");
+    
+        menuIcon.src = "assets/three-dot.svg";
+    
+        menu.appendChild(menuIcon);
+    
+        right.appendChild(menu);
+    
+        item.appendChild(left);
+        item.appendChild(right);
+    
+        completedContainer.appendChild(item);
+    
     });
 
 }
@@ -115,7 +362,29 @@ function updateTask() {
 
 }
 
-function completeTask() {
+function completeTask(id) {
+
+    const task = appData.tasks.find(
+        item => item.id === id
+    );
+
+    if (!task) return;
+
+    task.completed = !task.completed;
+
+    saveStorage();
+
+    renderTask();
+
+}
+
+function toggleCompleted(){
+
+    completedContainer.classList.toggle("show");
+
+    completedAccordion
+        .querySelector(".chevron-icon")
+        .classList.toggle("rotate");
 
 }
 
@@ -150,4 +419,8 @@ function renderPrioritySelect() {
 // EVENT
 // =========================
 
-// event listener
+document.addEventListener("click", () => {
+
+    taskMenu.classList.remove("show");
+
+});
